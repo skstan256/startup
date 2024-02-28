2cebc9a87f7498236f1bb790dbc9a2746afd72c3
Sarah <skstanley256>
2024-02-28 11:10:22 -0700

    Misc

--- project_combined.js
+++ project_combined.js
@@ -7,6 +7,20 @@ function demoReminder() {
     window.alert("Reminder: Commit your project! (3:30pm)")
 }
 
+function setDemoThoughts() {
+
+}
+
+
+function addThought() {
+    // select thought group
+    const thoughtText = document.querySelector("#thought-field")
+    const thought = document.createElement('li')
+    thought.className = "list-group-item";
+    thought.innerHTML = `<span class="list-group-item-text">${thoughtText}</span><menu class="list-group-item-controls"><button type="button" class="btn btn-link"><i class="bi bi-pencil-square"></i></button><button type="button" class="btn btn-link"><i class="bi bi-trash"></i></button></menu>`;
+    // appendChild to thought group
+}
+
 function projectOnLoad() {
     setUsername()
     demoReminder()
