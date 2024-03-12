f8877a0291462148e41fe8916df22c62b716c4a4
Sarah <skstanley256>
2024-03-11 21:53:22 -0600

    Add app.listen

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const express = require('express');
 const app = express();
+
+app.get('/*', (req, res) => {
+    res.send({ url: req.originalUrl });
+  });
+  
+const port = 3000;
+app.listen(port, function () {
+    console.log(`Listening on port ${port}`);
+    });
