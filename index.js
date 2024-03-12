562e3f217e15a5ebe86bcd24d03e27fbb4024c50
Sarah <skstanley256>
2024-03-11 22:02:15 -0600

    Move frontend to public folder

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.get('/*', (req, res) => {
-    res.send({ url: req.originalUrl });
-  });
+//app.get('/*', (req, res) => {
+    //res.send({ url: req.originalUrl });
+  //});
   
 const port = 3000;
 app.listen(port, function () {
--- login.js
+++
@@ -1,6 +0,0 @@
-function login() {
-    const nameEl = document.querySelector("#username");
-    localStorage.setItem("username", nameEl.value);
-    window.location.href = "home.html";
-}
-
