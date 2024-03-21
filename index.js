a08f60d048dd0a3904c1afa30f917d72fda1bbcd
Sarah <skstanley256>
2024-03-20 21:10:25 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const app = express();
 
+
+
+
 //app.get('/*', (req, res) => {
     //res.send({ url: req.originalUrl });
   //});
-  
+ 
+
+// JSON body parsing
+app.use(express.json());
+
+// serve up front end static content hosting
+app.use(express.static('public'));
+
+
+
+// go to default page if path is unknown
+app.use((_req, res) => {
+  res.sendFile('index.html', {root: 'public'});
+});
+
 const port = 3000;
 app.listen(port, function () {
     console.log(`Listening on port ${port}`);
