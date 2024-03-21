0b2d4db96e628d1a5f59f8942be4a5299f53471c
Sarah <skstanley256>
2024-03-20 21:20:17 -0600

    Configure for a new user (delete old one)

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const { MongoClient } = require('mongodb');
 
 const config = require('./dbconfig.json');
-const url = `mongodb+srv://cs260:cs260password@cluster0.ffl8ijs.mongodb.net/`;
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
