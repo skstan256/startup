e9babb990524ec2f2357b1cded9fe5306847d7d6
Sarah <skstanley256>
2024-03-25 23:19:47 -0600

    Add database functions in database.js

--- database.js
+++ database.js
@@ -1,12 +1,16 @@
 const { MongoClient } = require('mongodb');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
 const config = require('./dbconfig.json');
+
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
+const db = client.db('startup')
 
+const credentials = db.collection('credentials');
+const userData = db.collection('userData');
 
-const db = client.db('webbrain');
-const userCollection = db.collection('users');
-
+// REMEMBER TO ADD NEW FUNCTIONS TO MODULE.EXPORTS AT THE BOTTOM
 
 (async function testConnection() {
    await client.connect();
@@ -16,4 +20,50 @@ const userCollection = db.collection('users');
    process.exit(1);
 });
 
+
+function getUser(username) {
+   return credentials.findOne({username: username});
+}
+
+function getUserByToken(token) {
+   return credentials.findOne({token: token});
+}
+
+async function createUser(username, password) {
+   // create a document containing user data
+   const userDataDoc = {};
+   await userData.insertOne(userDataDoc);
+   const userDataID = userDataDoc._id;
+
+   // hash the password before inserting into database
+   const passwordHash = await bcrypt.hash(password, 10);
+
+   const user = {
+      username: username,
+      password: passwordHash,
+      userDataID: userDataID,
+      token: uuid.v4(),
+   };
+
+   await credentials.insertOne(user);
+   
+   return user;
+}
+
+// TODO: figure out if checking token here would be better (more secure)
+async function getUserData(userDataID) {
+   return userData.findOne({_id: userDataID})
+}
+
+async function updateUserData(userDataID) {
+
+}
+
+
+module.exports = {
+   getUser,
+   getUserByToken,
+   createUser,
+   getUserData,
+}
 
--- insertTestDatabaseData.js
+++ insertTestDatabaseData.js
@@ -6,7 +6,7 @@ async function main() {
     const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
     const client = new MongoClient(url);
     const db = client.db('startup');
-    const userCollection = db.collection('users');
+    const userCollection = db.collection('userData');
     
     (async function testConnection() {
         await client.connect();
@@ -21,9 +21,9 @@ async function main() {
             {
                 name: 'cs260',
                 thought_log: [
-                    {text: ''},
-                    {text: ''},
-                    {text: ''}
+                    {text: "We're no strangers to love"},
+                    {text: "You know the rules, and so do I"},
+                    {text: "You wouldn't get this from any other guy"}
                 ],
                 to_do_list: [
                     {text: 'Deploy Simon', completed: false},
