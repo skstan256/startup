885c5865227b9f3e3ae1fb50f130c39a20d6862b
Sarah <skstanley256>
2024-04-12 21:36:20 -0600

    Header at top and footer at bottom

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,51 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-    return <div className='body bg-dark text-light'>App will display here</div>
+    return (
+        <div className='body bg-dark text-light'>
+        <header className='container-fluid'>
+          <nav className='navbar fixed-top navbar-dark'>
+            <div className='navbar-brand'>
+              Simon<sup>&reg;</sup>
+            </div>
+            <menu className='navbar-nav'>
+              <li className='nav-item'>
+                <a className='nav-link' href='index.html'>
+                  Home
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a className='nav-link' href='play.html'>
+                  Play
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a className='nav-link' href='scores.html'>
+                  Scores
+                </a>
+              </li>
+              <li className='nav-item'>
+                <a className='nav-link' href='about.html'>
+                  About
+                </a>
+              </li>
+            </menu>
+          </nav>
+        </header>
+  
+        <main>App components go here</main>
+  
+        <footer className='bg-dark text-white-50'>
+          <div className='container-fluid'>
+          <hr />
+                <span className="bottom">Sarah Stanley</span>
+                <br />
+                <a href="https://github.com/skstan256/startup">Github</a>
+                <div>
+                   Inspirational quotes provided by <a href="https://zenquotes.io/" target="_blank">ZenQuotes API</a>
+                </div>
+          </div>
+        </footer>
+      </div>
+    );
 }
